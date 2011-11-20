8ea82258943024384e44cfc2647aa215937599fd
Viktorija <vik@thx.(none)>
2011-11-20 15:01:43 +0100

    OpenCV_1.cpp

---
+++ OpenCV_1.cpp
@@ -0,0 +1,23 @@
+#include <opencv2/core/core.hpp> 
+#include <opencv2/highgui/highgui.hpp>
+
+
+int main(int argc, char* argv[])
+{
+
+	IplImage* img = cvLoadImage("input_img.jpg");
+	CvSize size = cvGetSize(img);
+	cvRectangle(img, cvPoint(size.width/2-50, size.height/2-50), cvPoint(size.width/2+50, size.height/2+50), cvScalar(50, 200, 50), -1);
+	cvSaveImage("output_img.jpg", img);
+	cvNamedWindow("Image");
+	cvShowImage("Image", img);
+	cvWaitKey();
+	cvReleaseImage(&img);
+	cvDestroyWindow("Image");
+	return 0;
+}
+
+
+
+
+
