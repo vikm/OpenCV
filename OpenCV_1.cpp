b22a830d3470c444333fe54ef9ae19d429ad57a6
Viktorija <vik@thx.(none)>
2011-11-20 17:56:14 +0100

    VC++ project

--- OpenCV_1.cpp
+++ OpenCV_1.cpp
@@ -5,12 +5,26 @@
 int main(int argc, char* argv[])
 {
 
+	// load an image
 	IplImage* img = cvLoadImage("input_img.jpg");
+	// get image size
 	CvSize size = cvGetSize(img);
-	cvRectangle(img, cvPoint(size.width/2-50, size.height/2-50), cvPoint(size.width/2+50, size.height/2+50), cvScalar(50, 20	0, 50), -1);
+
+	// draw a square
+	cvRectangle(img, 
+		//calculate position of centered 100x100px square
+		cvPoint(size.width/2-50, size.height/2-50), 
+		cvPoint(size.width/2+50, size.height/2+50),
+		// white color filled square
+		cvScalar(255,255,255), -1);
+
+	// save new image
 	cvSaveImage("output_img.jpg", img);
+
+	// open a window and show the new image
 	cvNamedWindow("Image");
 	cvShowImage("Image", img);
+
 	cvWaitKey();
 	cvReleaseImage(&img);
 	cvDestroyWindow("Image");
